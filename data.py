271a8874f7fc87ff3e458852ecba0190cf7ae937
adejumoridwan <adejumo999@gmail.com>
2022-11-01 09:09:48 +0100

    Update

--- data.py
+++ data.py
@@ -1,7 +1,10 @@
 #importing required libraries
 import streamlit as st
 import pandas as pd
+import seaborn as sns
 
+
+st.sidebar.header("Upload Data")
 
 # Allow only .csv and .xlsx files to be uploaded
 uploaded_file = st.sidebar.file_uploader("Upload CSV or Excel File", type=["csv", "xlsx"])
@@ -13,15 +16,25 @@ if uploaded_file:
         data = pd.read_excel(uploaded_file, engine = "openpyxl")
     else:
         data= pd.read_csv(uploaded_file)
-        
 
-    Data, Descriptive = st.tabs(["Data", "Descriptive"])
-
+    Data, Descriptive, Visualization, Inferential = st.tabs(["Data", "Descriptive","Visualization","Inferential"])
+    
     Data.dataframe(data)
 
     Descriptive.header("Descriptive Statistics")
 
     Descriptive.table(data.describe())
+
+    data_variables = data.columns
+
+    st.sidebar.header("Visualization Controls")
+
+    st.sidebar.selectbox('Select X-axis', data_variables)
+
+    st.sidebar.selectbox('Select Y-axis', data_variables)
+
+    st.sidebar.selectbox('Select Group', data_variables)
+
     
 
 #retun message if no file is uploaded
