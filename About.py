d913c9390138cfe9024991f6b893c2da8bb5bda2
adejumoridwan <adejumo999@gmail.com>
2022-12-16 14:16:01 +0100

    added readme

---
+++ About.py
@@ -0,0 +1,34 @@
+import streamlit as st
+
+st.header("About QuickStats")
+st.write(
+    '''
+QuickStats is a web app built using the streamlit framework.It is designed to provide users with a quick and easy way to perform statistical and exploratory data analysis on their datasets. \n
+With QuickStats, users can easily upload their data, select the appropriate analysis methods, and view the results in an intuitive and interactive format. The app offers a wide range of statistical tools and visualization options, including histograms, scatter plots, and box plots, as well as advanced techniques such as regression analysis and hypothesis testing.
+QuickStats is ideal for users who want to quickly and easily explore their data and gain insights into their data patterns and trends. The app is user-friendly and intuitive, making it accessible to users of all skill levels.\n
+Whether you are a data scientist, a business analyst, or a student, QuickStats is the perfect tool for performing efficient and effective data analysis. Give it a try today and see how it can help you uncover valuable insights from your data.\n
+Source Code is available [here](https://github.com/adejumoridwan/QuickStats.git)
+    ''',
+    unsafe_allow_html=True
+)
+
+st.header("About The Author")
+st.write(
+    '''
+Hi there, I'm Adejumo Ridwan Suleiman and I'm a statistician and data scientist. I have a wealth of experience using R and Python for data science and have been a data science instructor for several years.\n
+I have always been passionate about statistics and data analysis and decided to pursue a career in this field early on. I hold a Bachelor's degree in Statistics and I have freelanced for a variety of companies and organizations, using my skills to help them make sense of their data and make informed decisions.\n
+One of the things that I enjoy most about my work is teaching others how to use R and Python for data science. I have taught courses on these topics at various workshops and webinars, and I am always thrilled to see my students develop their skills and gain confidence in their abilities.\n
+In my free time, I enjoy exploring new technologies and tools in the field of data science, as well as staying up to date on the latest developments in the field. I am always looking for new ways to help others learn and grow in this exciting and rapidly evolving field.\n
+You can find more information about me on my LinkedIn profile <a href='https://www.linkedin.com/in/adejumoridwan/'>
+        <img src='https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white' />
+    </a>\n
+I am also active on <a href='https://twitter.com/abuuAishaaa'>
+        <img src='https://img.shields.io/badge/Twitter-1DA1F2?style=for-the-badge&logo=twitter&logoColor=white' />
+    </a>where I share thoughts and updates about my projects, as well as industry news and insights\n
+In addition to my technical pursuits, I enjoy writing about my experiences and learning journey as a data scientist on my Medium blog
+<a href='https://medium.com/@adejumo999'>
+        <img src='https://img.shields.io/badge/Medium-12100E?style=for-the-badge&logo=medium&logoColor=white' />
+    </a>
+''',
+unsafe_allow_html=True
+)
